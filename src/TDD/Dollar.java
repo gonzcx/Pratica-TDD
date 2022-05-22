b41b32fe48e2dd5fcd2fde8cf26902db180c7c58
joaogonzales <joaovitorgonzales@gmail.com>
2022-05-22 18:47:30 -0300

    commit 2

--- src/TDD/Dollar.java
+++ src/TDD/Dollar.java
@@ -1,16 +1,48 @@
 package TDD;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
-public class Dollar {
-    int amount = 10;
-    Dollar(int amount) {}
-    void times(int multiplier) {}
+    class Dollar extends Money{
+    Dollar(int amount) {
+        this.amount= amount;
+    }
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
 
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
+    }
+}
+
+class Franc extends Money{
+    Franc(int amount) {
+        this.amount= amount;
+    }
+    Franc times(int multiplier)  {
+        return new Franc(amount * multiplier);
+    }
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+}
+
+class Money  {
+    protected int amount;
+
+    public boolean equals(Object object)  {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
 
