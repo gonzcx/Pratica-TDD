2d7e0708f496d67f24e70439d809ab717a97c1ba
joaogonzales <joaovitorgonzales@gmail.com>
2022-05-22 19:10:12 -0300

    commit final

--- src/TDD/Dollar.java
+++ src/TDD/Dollar.java
@@ -2,48 +2,16 @@ package TDD;
 
 import static org.junit.Assert.*;
 
-    class Dollar extends Money{
-        Dollar(int amount, String currency)  {
-            super(amount, currency);
-        }
-        String currency() {
-            return currency;
-        }
-
-        Money times(int multiplier)  {
-            return Money.dollar(amount * multiplier);
-        }
-
-}
-
-class Franc extends Money{
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-
-    String currency() {
-        return currency;
-    }
-    Money times(int multiplier)  {
-        return Money.franc(amount * multiplier);
-    }
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-}
-
- abstract class Money  {
+    class Money  {
     protected int amount;
     protected String currency;
 
      static Money dollar(int amount)  {
-         return new Dollar(amount, "USD");
+         return new Money(amount, "USD");
      }
 
      static Money franc(int amount) {
-         return new Franc(amount, "CHF");
+         return new Money(amount, "CHF");
      }
 
      Money(int amount, String currency) {
@@ -52,12 +20,14 @@ class Franc extends Money{
      }
 
 
-     abstract Money times(int multiplier);
+     Money times(int multiplier) {
+         return new Money(amount * multiplier, currency);
+     }
 
-    public boolean equals(Object object) {
-        Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
-    }
+     public boolean equals(Object object) {
+         Money money = (Money) object;
+         return amount == money.amount && currency().equals(money.currency());
+     }
 
      String currency() {
          return currency;
@@ -66,23 +36,13 @@ class Franc extends Money{
      public void testEquality() {
          assertTrue(Money.dollar(5).equals(Money.dollar(5)));
          assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-         assertTrue(Money.franc(5).equals(Money.franc(5)));
-         assertFalse(Money.franc(5).equals(Money.franc(6)));
          assertFalse(Money.franc(5).equals(Money.dollar(5)));
      }
-
-
-     public void testMultiplication() {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
-    }
 
      public void testCurrency() {
          assertEquals("USD", Money.dollar(1).currency());
          assertEquals("CHF", Money.franc(1).currency());
      }
-
 
  }
 
